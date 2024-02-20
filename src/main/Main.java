0210311a642f94f81c0fb619bd49610239d0cfd2
Jiří Loukota <jloukota@seznam.cz>
2024-02-20 17:21:26 +0100

    Moved class Main into main package and changed testing data.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,14 @@
+package main;
+
+import exceptions.PlantException;
+import plant.Plant;
+
 public class Main {
     public static void main(String[] args) {
-        Plant plant = null;
+        Plant plant;
         try {
             plant = new Plant("Rose");
+            System.out.println(plant.getWateringInfo());
         } catch (PlantException e) {
             System.err.println(e.getLocalizedMessage());
         }
