f9b398cb6a47f73b78d49a7f5011f0358d0e8bcb
Jiří Loukota <jloukota@seznam.cz>
2024-02-21 18:00:42 +0100

    Changed message thrown by PlantException.

--- src/exceptions/PlantException.java
+++ src/exceptions/PlantException.java
@@ -1,6 +1,6 @@
 package exceptions;
 public class PlantException extends Exception{
     public PlantException(String message){
-        super(message);
+        super("Plant exception: " + message);
     }
 }
