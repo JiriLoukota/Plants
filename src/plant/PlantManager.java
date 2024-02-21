4d750cb66311ea5173867558de430d6a68844e13
Jiří Loukota <jloukota@seznam.cz>
2024-02-21 20:17:59 +0100

    Added new methods into PlantManager, adjusted imports and changed the format of the code.

--- src/plant/PlantManager.java
+++ src/plant/PlantManager.java
@@ -1,74 +1,32 @@
 package plant;
-import exceptions.FileException;
-import exceptions.PlantException;
 
-import java.io.*;
+import comparators.WateringDateComparator;
+import exceptions.FileException;
+
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.util.*;
 
 public class PlantManager {
     //Attributes
     private List<Plant> plantList;
     private String path;
+
     //Constructors
     public PlantManager(List<Plant> plantList) {
         this.plantList = plantList;
     }
+
     public PlantManager(String path) throws FileException {
         loadFromFile(path);
     }
-    //Special methods
-    public void addPlant(Plant plant){
-        plantList.add(plant);
-    }
-    public void addPlants(List<Plant> plants){
-        plantList.addAll(plants);
-    }
-    public Plant get(int index){
-        return plantList.get(index);
-    }
-    public void remove(int index){
-        plantList.remove(index);
-    }
 
-    public void loadFromFile(String path) throws FileException {
-        if(plantList!=null)plantList.clear();
-        else plantList = new ArrayList<>();
-        Scanner scanner;
-        try {
-            scanner = new Scanner(new BufferedReader(new FileReader(path)));
-        } catch (FileNotFoundException e) {
-            throw new FileException("Couldn't load file from location: " + path);
-        }
-        while(scanner.hasNextLine()){
-            String line = scanner.nextLine();
-            String[] parts = line.split("\t");
-            if(parts.length==5){
-                try {
-                    Plant plant = new Plant(parts[0], parts[1],LocalDate.parse(parts[4]), LocalDate.parse(parts[3]), Integer.parseInt(parts[2]));
-                    plantList.add(plant);
-                } catch (PlantException e) {
-                    throw new FileException("Couldn't read data from file: " + path + " because it is corrupted.");
-                }
-            }
-        }
-        this.path = path;
-        scanner.close();
-        //loading from file, throws exception, one list per one file
-    }
-    public void updateFile() throws FileException {
-        try (PrintWriter printWriter = new PrintWriter(new FileWriter(path))){
-            for (Plant plant : plantList) {
-                System.out.println(plant.toSavingFormat());
-                printWriter.println(plant.toSavingFormat());
-            }
-        } catch (IOException e) {
-            throw new FileException("Couldn't write to file: " + path + "because it couldn't be opened,  or it doesn't exist.");
-        }
-    }
-
+    //Getters and setters
     public List<Plant> getPlantList() {
         return plantList;
     }
@@ -76,4 +34,82 @@ public class PlantManager {
     public void setPlantList(List<Plant> plantList) {
         this.plantList = plantList;
     }
+    //region Special methods
+
+    //Adding Plant to end of the list
+    public void addPlant(Plant plant) {
+        plantList.add(plant);
+    }
+
+    //Adding list of Plants into the list
+    public void addPlants(List<Plant> plants) {
+        plantList.addAll(plants);
+    }
+
+    //Getting Plant from index
+    public Plant get(int index) {
+        return plantList.get(index);
+    }
+
+    //Removing Plant from index
+    public void remove(int index) {
+        plantList.remove(index);
+    }
+
+    //Sorting list by name
+    public void sortByName() {
+        Collections.sort(plantList);
+    }
+
+    //Sorting list by oldest last watering
+    public void sortByLastWatering() {
+        plantList.sort(new WateringDateComparator());
+    }
+
+    //Loading list from file
+    public void loadFromFile(String path) throws FileException {
+        if (plantList != null) plantList.clear();
+        else plantList = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(path)))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split("\t");
+                if (parts.length == 5) {
+                    try {
+                        Plant plant = new Plant(parts[0], parts[1], LocalDate.parse(parts[4]), LocalDate.parse(parts[3]), Integer.parseInt(parts[2]));
+                        plantList.add(plant);
+                    } catch (Exception e) {
+                        throw new FileException("Couldn't read data from file: " + path + " because it is corrupted.");
+                    }
+                }
+            }
+            this.path = path;
+        } catch (FileNotFoundException e) {
+            throw new FileException("Couldn't load file from location: " + path);
+        }
+    }
+
+    //Saving list into the file it was loaded from
+    public void updateFile() throws FileException {
+        try (PrintWriter printWriter = new PrintWriter(new FileWriter(path))) {
+            for (Plant plant : plantList) {
+                printWriter.println(plant.toSavingFormat());
+            }
+        } catch (IOException e) {
+            throw new FileException("Couldn't write to file: " + path + "because it couldn't be opened,  or it doesn't exist.");
+        }
+    }
+
+    //Saving list into defined file
+    public void saveToFile(String path) throws FileException {
+        this.path = path;
+        try (PrintWriter printWriter = new PrintWriter(new FileWriter(path))) {
+            for (Plant plant : plantList) {
+                printWriter.println(plant.toSavingFormat());
+            }
+        } catch (IOException e) {
+            throw new FileException("Couldn't write to file: " + path + "because it couldn't be opened,  or it doesn't exist.");
+        }
+    }
+    //endregion
 }
